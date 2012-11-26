3e64c1c63fc152d55e1b464c5aa88e22a0266e5f
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-11-26 16:35:48 +0100

    attempt to fix order.

--- manifests/bucket.pp
+++ manifests/bucket.pp
@@ -1,9 +1,10 @@
 define couchbase::bucket($port, $size, $user='couchbase', $password='password', $type='memcached', $replica=1) {
-    Class['couchbase'] -> Couchbase::Bucket[$title]
+    Class['couchbase::install'] -> Class['couchbase::config'] -> Class['couchbase::service'] -> Couchbase::Bucket[$title]
     
     exec {"bucket-create-$title":
         path => '/opt/couchbase/bin/',
         command => "couchbase-cli bucket-create -c localhost -u $user -p $password --bucket=$title --bucket-type=$type --bucket-ramsize=$size --bucket-port=$port --bucket-replica=$replica",
+        require => Class['couchbase::config'],
         returns => [0, 2]
     }
 }
