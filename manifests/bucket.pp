499fa85c70dd769d0033f60ae86068e18f3b9fb4
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-11-26 15:51:35 +0100

    typo

--- manifests/bucket.pp
+++ manifests/bucket.pp
@@ -2,7 +2,7 @@ define bucket($port, $size, $user='couchbase', $password='password', $type='memc
     exec {"bucket-create-$title":
         path => '/opt/couchbase/bin/',
         require => Class['couchbase'],
-        command => "couchbase-cli bucket-create -c localhost -u $user -p $password --bucket=$title --bucket-type=$type --bucket-ramsize=$size --bucket-port=$port --bucket-replica=$replica"
+        command => "couchbase-cli bucket-create -c localhost -u $user -p $password --bucket=$title --bucket-type=$type --bucket-ramsize=$size --bucket-port=$port --bucket-replica=$replica",
         returns => [0, 2]
     }
 }
