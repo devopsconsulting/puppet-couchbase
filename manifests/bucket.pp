046b0c4b29b812b836ed4189c42cec2fc9bfee94
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-11-27 14:45:42 +0100

    cleanup.

--- manifests/bucket.pp
+++ manifests/bucket.pp
@@ -1,6 +1,10 @@
 define couchbase::bucket($port, $size, $user='couchbase', $password='password', $type='memcached', $replica=1) {
-    Class['couchbase::install'] -> Class['couchbase::config'] -> Class['couchbase::service'] -> Couchbase::Bucket[$title]
-    
+
+    # all this has to be done before we can create buckets.
+    Class['couchbase::install'] -> Couchbase::Bucket[$title]
+    Class['couchbase::config'] -> Couchbase::Bucket[$title]
+    Class['couchbase::service'] -> Couchbase::Bucket[$title]
+
     exec {"bucket-create-$title":
         path => ['/opt/couchbase/bin/', '/usr/bin/'],
         command => "couchbase-cli bucket-create -c localhost -u $user -p $password --bucket=$title --bucket-type=$type --bucket-ramsize=$size --bucket-port=$port --bucket-replica=$replica",
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,16 +2,14 @@ class couchbase($size=1024, $user='couchbase', $password='password', $version=la
     
     class {"couchbase::install":
         version => $version
-    }
+        } ->
     
     class {"couchbase::config":
         size => $size,
         user => $user,
         password => $password,
         peers => $peers
-    }
+        } ->
     
-    include couchbase::service
-    
-    Class['couchbase::install'] -> Class['couchbase::config'] -> Class['couchbase::service']
+    class {"couchbase::service":}
 }
